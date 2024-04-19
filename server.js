c089a72e17108a77fe72aebcf2db166af35bcb35
Sahil Dholariya <98894260+Sahildholariya@users.noreply.github.com>
2024-04-19 15:56:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require("express")
 const app = express()
 const connectDB = require('./db/connect')
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 1000;
 
 const product_routes = require("./routes/product")
 
